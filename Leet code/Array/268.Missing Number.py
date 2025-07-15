5ed85c09387232b9b56fce3eac2e873d196b1a16
AathilAli <162690765+AathilAli@users.noreply.github.com>
2025-07-15 12:49:44 +0530

    Update 268.Missing Number.py

--- Leet code/Array/268.Missing Number.py
+++ Leet code/Array/268.Missing Number.py
@@ -1,10 +1,9 @@
 class Solution(object):
     def missingNumber(self, nums):
-        sum=0
-        total=0
+        expected_sum=0
+        actual_sum=0
         n=len(nums)+1
-        sum=(n*(n-1)) // 2
+        expected_sum=(n*(n-1)) // 2
         for i in nums:
-            total+=i
-        print(total ,sum)
-        return sum - total
+            actual_sum+=i
+        return expected_sum - actual_sum
